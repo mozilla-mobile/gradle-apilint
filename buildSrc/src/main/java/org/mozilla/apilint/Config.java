09f7bb0b991dcc0751f1833e0f5ee55bad57388d
Agi Sferro <agi@mozilla.com>
2019-04-02 13:23:52 -0700

    Branch 0.1.9

--- buildSrc/src/main/java/org/mozilla/apilint/Config.java
+++ buildSrc/src/main/java/org/mozilla/apilint/Config.java
@@ -5,6 +5,6 @@
 package org.mozilla.apilint;
 
 public class Config {
-    public final static String API_DOC_VERSION = "master-SNAPSHOT";
+    public final static String API_DOC_VERSION = "0.1.9";
     public final static String GROUP = "org.mozilla.apilint";
 }
